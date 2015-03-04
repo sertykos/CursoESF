c415414aa7162bdc062d6cb39751af4466ab2642
sertykos <sertykos@gmail.com>
2015-03-04 17:30:40 +0100

    1.0

---
+++ WebEmpleados/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebEmpleados.Models;
+
+namespace WebEmpleados.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            using (var db = new EmpleadosEntities())
+            {
+                return View(db.Empleados.ToList());
+            }          
+        }
+
+        public ActionResult Alta()
+        {
+            return View(new Empleados());
+        }
+
+        [HttpPost]
+        public ActionResult Alta(Empleados model)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new EmpleadosEntities())
+                {
+                    db.Empleados.Add(model);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(model);
+        }
+    }
+}
---
+++ WebEmpleados/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebEmpleados.MvcApplication" Language="C#" %>
